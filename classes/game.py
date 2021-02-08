b4fc40ae9535682eac2212a72a67b11df2754d01
Eden Porte <eden.porte@iut2.univ-grenoble-alpes.fr>
2021-02-08 12:01:29 +0100

    boollean jour nuit

--- classes/game.py
+++ classes/game.py
@@ -7,23 +7,18 @@ class Game:
         self.background = pygame.image.load('assets/1.png')
         self.is_playing = False
         self.pressed = {} # Représente les touches actuellement enfoncées par le joueur
-        self.etape = 1 # 1 représente le premier jour
-
+        self.etape = 1 # 1 représente le premier jour, 2 le deuxième, 3 le troisième, et les suivants représentent les cinématiques de fin
+        self.jour = False # indique s'il fait jour ou nuit, au début du jeu, on est sur la phase Jour 
+        self.phaseDeJeu = None
 
     def drawJeu(self,screen):
         screen.blit(self.background, (0, 0))
-        if self.etape % 2 == 0 and self.cauchemar == None:
-            self.cauchemar = Cauchemar(self.etape, self.visualNovelPart)
-        elif self.etape % 2 == 0:
-            self.cauchemar.drawCauchemar(screen)
+        if not self.jour and self.phaseDeJeu == None:
+            self.phaseDeJeu = Cauchemar(self.etape, self.phaseDeJeu)
+        elif not self.jour:
+            self.phaseDeJeu.drawCauchemar(screen)
 
     def catch_signal(self,event):
-        # On mets à jour pressed
-        if event.type == pygame.KEYDOWN:
-            self.pressed[event.key] = True
-        if event.type == pygame.KEYUP:
-            self.pressed[event.key] = False
-
         # On effectue les actions du jeu en fonction de l'endroit où nous sommes
-        if self.etape % 2 == 0 and self.cauchemar != None:
-            self.cauchemar.catch_signal(self.pressed)
+        if not self.jour and self.phaseDeJeu != None:
+            self.phaseDeJeu.catch_signal(self.pressed)
