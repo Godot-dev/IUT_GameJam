33c5bd1569d6ea89ee0f026ba72b626cc23933de
William <>
2021-02-11 22:14:13 +0100

    ajout btn menu

--- classes/menu.py
+++ classes/menu.py
@@ -8,12 +8,17 @@ class Menu:
         self.playButton =    Button(260, 107, 382, 300, 'assets/Menu/Jouer.png')
         self.creditsButton = Button(260, 107, 382, 425, 'assets/Menu/Credits.png')
         self.quitButton =    Button(260, 107, 382, 550, 'assets/Menu/quitter.png')
+        self.HighScore =    Button(180, 62, 820, 680, 'assets/Menu/HighScore.png')
+        self.SandBox =    Button(180, 62, 25, 680, 'assets/Menu/SandBox.png')
 
     def drawMenu(self,screen): # On affiche le menu, chaque bouton est positionné au rectangle formé par sa hitbox
         screen.blit(self.background, (0, 0))
-        screen.blit(self.playButton.img, self.playButton.hitbox)
+        screen.blit(self.playButton.img, self.playButton.hitbox)  
         screen.blit(self.creditsButton.img, self.creditsButton.hitbox)
         screen.blit(self.quitButton.img, self.quitButton.hitbox)
+        screen.blit(self.HighScore.img, self.HighScore.hitbox)
+        screen.blit(self.SandBox.img, self.SandBox.hitbox)
+        
 
     def catch_signal(self, game, credits, event): # Effectue l'action appropriée si l'utilisateur a cliqué sur un des boutons du menu
         if self.playButton.hitbox.collidepoint(event.pos):
