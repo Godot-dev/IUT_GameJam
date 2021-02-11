35ed157058440d43da05d02162d74180c2f81c7f
Eden Porte <eden.porte@iut2.univ-grenoble-alpes.fr>
2021-02-11 14:52:16 +0100

    resolve bug

---
+++ classes/credits.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Credits:
+    def __init__(self):
+        # Image de background
+        self.background = pygame.image.load('assets/background.png')
+        # Instanciation des éléments liés au menu des options
+        self.is_credits = False
+
+    def drawOptions(self,screen):
+        screen.blit(self.background, (0, 0))
+
+    def catch_signal(self,event):
+        pass
